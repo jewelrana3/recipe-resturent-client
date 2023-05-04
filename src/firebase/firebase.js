cf3aa7231e13329da6874e79765762e810bde794
jewelrana3 <juyelrana7752@gmail.com>
2023-05-05 00:03:21 +0600

    finish

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,15 +4,16 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+console.log('config variable',import.meta.env.VITE_apikey)
 const firebaseConfig = {
-  apiKey: "AIzaSyC5l-iAQuMYPOGLqwIKYt3SSlJqhSKK3Zc",
-  authDomain: "project-assignment-10.firebaseapp.com",
-  projectId: "project-assignment-10",
-  storageBucket: "project-assignment-10.appspot.com",
-  messagingSenderId: "633825013446",
-  appId: "1:633825013446:web:70e53bfcfd3cc8a88171c7"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export default app;
+export default app
